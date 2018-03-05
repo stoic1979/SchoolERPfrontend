3bc629796fc3e0368ea635e25dce5b22c774e5a8
Navjot Singh <weavebytes@gmail.com>
2018-03-06 03:07:58 +0530

    removed double module includes

--- src/app/ui-components/ui-components.module.ts
+++ src/app/ui-components/ui-components.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, SidebarComponent, FooterComponent]
+  declarations: []
 })
 export class UiComponentsModule { }
