71087fdf453600f887916cb911f1a7b9e57e5eed
Navjot Singh <weavebytes@gmail.com>
2018-03-06 01:28:07 +0530

    add express to render agular ui

---
+++ server.js
@@ -0,0 +1,9 @@
+//Install express server
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
