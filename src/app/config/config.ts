2b0e20f3bce5cb12ad744b7f99114c19a381af6b
Navjot Singh <weavebytes@gmail.com>
2018-03-06 03:21:29 +0530

    changed api url

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,6 @@
 const config = {
-	 API_ENDPOINT : 'http://localhost:3000',
+	 //API_ENDPOINT : 'http://localhost:3000',
+	 API_ENDPOINT : 'https://schoolerp-api.herokuapp.com'
 	 //const API_ENDPOINT = 'http://192.168.1.13:3000';
 }
 
