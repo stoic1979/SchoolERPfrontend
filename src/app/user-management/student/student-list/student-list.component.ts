bfe8aae536cde942f9614131576cff2f0311b96d
Navjot Singh <weavebytes@gmail.com>
2018-03-07 03:46:10 +0530

    loading spinner in student list component

--- src/app/user-management/student/student-list/student-list.component.ts
+++ src/app/user-management/student/student-list/student-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 
 import { StudentService } from '../../../core/services/user-management/student.service';
+import { LoadingService } from '../../../core/services/utils/loading.service';
+
 
 @Component({
   selector: 'app-student-list',
@@ -16,16 +18,19 @@ export class StudentListComponent implements OnInit {
   all;
 
   constructor(
-  private studentService: StudentService
+  private studentService: StudentService,
+  private loadingService: LoadingService
   ) { 
   }
 
   ngOnInit() {
-
+     this.loadingService.display(true);
      this.studentService.get().subscribe((res)=> {
+        this.loadingService.display(false);
         console.log('[Student List Component] Response =>' +JSON.stringify(res));
         this.dataSource = res.data;
       },(err) => {
+            this.loadingService.display(false);
             const errBody = err.json();
             console.log('add student  error: ', errBody);
       });
