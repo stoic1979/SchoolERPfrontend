99dc7f49ab14b167e771125a17af7abd1708fa38
Navjot Singh <weavebytes@gmail.com>
2018-03-07 03:50:21 +0530

    removed double inclusion of alert comp

--- src/app/ui-components/ui-components.module.ts
+++ src/app/ui-components/ui-components.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AlertComponent } from './alert/alert.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AlertComponent]
+  declarations: []
 })
 export class UiComponentsModule { }
