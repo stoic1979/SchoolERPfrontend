3f9eb02546218eb493720b6c2b14cfca5aa5777b
Roohi Verma <roohivermapb@gmail.com>
2018-03-09 16:57:41 +0530

    code for adding finance manager

--- src/app/user-management/finance-manager/finance-manager-form/finance-manager-form.component.ts
+++ src/app/user-management/finance-manager/finance-manager-form/finance-manager-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { AlertService } from '../../../core/services/utils/alert.service';
+import { LoadingService } from '../../../core/services/utils/loading.service';
+import { FinanceManagerService } from '../../../core/services/user-management/finance-manager.service';
 
 @Component({
   selector: 'app-finance-manager-form',
@@ -7,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinanceManagerFormComponent implements OnInit {
 
-  constructor() { }
+  public isRole: boolean = false;
+ 
+  form: FormGroup;
+
+  private formSubmitAttempt: boolean;
+
+  constructor(
+  private fb: FormBuilder,
+  private financeManagerService: FinanceManagerService,
+  private alertService: AlertService,
+  private loadingService: LoadingService
+  ) { }
 
   ngOnInit() {
+  this.form = this.fb.group({
+        name:  ['', Validators.required],
+        designation:  ['', Validators.required],
+        father_name:  ['', Validators.required],
+        section:  ['', Validators.required],
+        dob:  ['', Validators.required],
+        doj:  ['', Validators.required],
+        age:   ['', Validators.required],
+        aadhar_id:  ['', Validators.required],
+        domicile:  ['', Validators.required],
+        marital_status:   ['', Validators.required],
+        address:   ['', Validators.required],
+       	education:   ['', Validators.required],
+       	working_exp:   ['', Validators.required],
+        bank_name:   ['', Validators.required],
+        bank_acc_no:   ['', Validators.required],
+        password:   ['', Validators.required],
+    });
   }
+
+  onSubmit() {
+    console.log('onSubmit()');
+    console.log('finance data '+this.form.value);
+    this.loadingService.display(true);
+
+    this.financeManagerService.add(this.form.value).subscribe((res)=> {
+
+        this.loadingService.display(false);
+
+        console.log('[FinanceManagerFormComponent] Response =>' +JSON.stringify(res));
+        this.formSubmitAttempt = true;
+        this.alertService.success("Finance Manager added successfully");
+
+      },(err) => {
+            
+            this.loadingService.display(false);
+
+            const errBody = err.json();
+            console.log('add Finance Manager  error: ', errBody);
+            this.alertService.success("[FinanceManagerFormComponent] failed to add finance manager");
+      });
+    }
 
 }
