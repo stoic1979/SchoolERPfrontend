26922a06497b4baa65d0c537a98713979d0cd3bf
Roohi Verma <roohivermapb@gmail.com>
2018-03-09 16:58:24 +0530

    code for showing finance manager data in a table

--- src/app/user-management/finance-manager/finance-manager-list/finance-manager-list.component.ts
+++ src/app/user-management/finance-manager/finance-manager-list/finance-manager-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FinanceManagerService } from '../../../core/services/user-management/finance-manager.service';
+import { LoadingService } from '../../../core/services/utils/loading.service';
+
 @Component({
   selector: 'app-finance-manager-list',
   templateUrl: './finance-manager-list.component.html',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinanceManagerListComponent implements OnInit {
 
-  constructor() { }
+  dataSource;
+  options;
+  all;
+
+
+  constructor(
+  private financeManagerService: FinanceManagerService,
+  private loadingService: LoadingService
+  ) { }
 
   ngOnInit() {
+  this.loadingService.display(true);
+     this.financeManagerService.get().subscribe((res)=> {
+        this.loadingService.display(false);
+        console.log('[FinanceManagerListComponent] Response =>' +JSON.stringify(res));
+        this.dataSource = res.data;
+      },(err) => {
+            this.loadingService.display(false);
+            const errBody = err.json();
+            console.log('add financemanager  error: ', errBody);
+      });
   }
 
 }
