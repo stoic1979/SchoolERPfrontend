74e732e8f539ac79e26858f2137be0df9cab1fb1
Roohi Verma <roohivermapb@gmail.com>
2018-03-15 14:18:53 +0530

    code for student details page

--- src/app/user-management/student/student-details/student-details.component.ts
+++ src/app/user-management/student/student-details/student-details.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { StudentService } from '../../../core/services/user-management/student.service';
+import { AlertService } from '../../../core/services/utils/alert.service';
+import { LoadingService } from '../../../core/services/utils/loading.service';
+
+import { TabManager } from '../../../core/helpers/tabManager';
+
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
   styleUrls: ['./student-details.component.css']
 })
-export class StudentDetailsComponent implements OnInit {
+export class StudentDetailsComponent extends TabManager implements OnInit {
 
-  constructor() { }
+  public isRole: boolean = false;
+ 
+  form: FormGroup;
+
+  private formSubmitAttempt: boolean;
+
+  constructor(
+  	private fb: FormBuilder,
+    private studentService: StudentService,
+    private alertService: AlertService,
+    private loadingService: LoadingService
+  	) {
+  		super();
+  	 }
 
   ngOnInit() {
+
+  	// calling openTab from TabManager
+     this.openTab('student_tab');
+   }
   }
 
-}
+  
+
