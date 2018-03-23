45c472e23bcb5a33350344afc77bf3e7bfe48cf1
Roohi Verma <roohivermapb@gmail.com>
2018-03-23 17:28:33 +0530

    changed the date format

--- src/app/core/helpers/utils.ts
+++ src/app/core/helpers/utils.ts
@@ -2,6 +2,9 @@ export default class Utils {
 
   static dateOnlyStr(dt: string) {
     const d: Date = new Date(dt);
-    return d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
+    const mm = d.getMonth()+1;
+    const dd = d.getDate();
+
+    return [(dd>9 ? '' : '0') + dd,(mm>9 ? '' : '0') + mm,d.getFullYear()].join('-');
   }
 }
