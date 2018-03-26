b1eaa8acea977dcaed1fe532ff8e6446b582a749
Navjot Singh <weavebytes@gmail.com>
2018-03-26 16:31:38 +0530

    Created Attendance and Stationery Services

--- src/app/core/services/system-management/attendance.service.ts
+++ src/app/core/services/system-management/attendance.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Http, Headers } from '@angular/http';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+const API_ENDPOINT = 'http://localhost:3000';
 
 @Injectable()
 export class AttendanceService {
 
-  constructor() { }
+	result: any;
 
-}
+  constructor(
+  	private http: Http,
+  	private router: Router
+  	) { }
+
+  add = (credential: any) => {
+  	const header = this.createAuthorizationHeader();
+    return this.http.post(`${API_ENDPOINT}/api/attendance`, credential, { headers: header })
+      .map(res => this.result = res.json());        
+  }
+  getToken () {
+  	console.log( '[SectionService] token '+localStorage.getItem('userToken'));
+  	return localStorage.getItem('userToken') || '';
+  }
+
+  private handleError (error: Response | any ) {
+  	console.error('[SectionService] :: handleError', error);
+  	return Observable.throw(error);
+  }
+
+  createAuthorizationHeader () {
+  	const headers = new Headers();
+  	const token = this.getToken();
+  	headers.append('Content-Type', 'application/json; charset=utf-8');
+  	headers.append('Authorization', token);
+  	return headers;
+  }
+
+}//AttendanceService
